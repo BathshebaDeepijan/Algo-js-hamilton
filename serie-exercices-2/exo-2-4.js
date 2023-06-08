d4cae3ce1bcba38f03e2d0d30d120e806363c3b2
bathsheba deepijan <deepijanbathsheba@gmail.com>
2023-06-08 16:36:46 +0200

    avancement serie 2

---
+++ serie-exercices-2/exo-2-4.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      let resultatDivision = i / 2;
+      console.log("Le résultat de la division de " + i + " par 2 est : " + resultatDivision);
+    } else {
+      let resultatMultiplication = i * 3;
+      console.log("Le résultat de la multiplication de " + i + " par 3 est : " + resultatMultiplication);
+    }
+  }
+  
